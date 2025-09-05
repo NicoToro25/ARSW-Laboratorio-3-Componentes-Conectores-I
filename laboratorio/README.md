f97a74b67e0ac0d9c5392ca1f33bbcd78a9628ec
NicoToro25 <nikotoro25@gmail.com>
2025-09-05 15:27:20 -0500

    laboratorio completo

--- laboratorio/README.md
+++ laboratorio/README.md
@@ -34,8 +34,12 @@ Mostrar InMemoryPersistence, BlueprintsServices y test de los nuevos métodos.
 
 3. Haga un programa en el que cree (mediante Spring) una instancia de BlueprintServices, y rectifique la funcionalidad del mismo: registrar planos, consultar planos, registrar planos específicos, etc.
 
+Se muestra el Main, AppConfig.
+
 4. Se quiere que las operaciones de consulta de planos realicen un proceso de filtrado, antes de retornar los planos consultados. Dichos filtros lo que buscan es reducir el tamaño de los planos, removiendo datos redundantes o simplemente submuestrando, antes de retornarlos. Ajuste la aplicación (agregando las abstracciones e implementaciones que considere) para que a la clase BlueprintServices se le inyecte uno de dos posibles 'filtros' (o eventuales futuros filtros). No se contempla el uso de más de uno a la vez:
 	* (A) Filtrado de redundancias: suprime del plano los puntos consecutivos que sean repetidos.
 	* (B) Filtrado de submuestreo: suprime 1 de cada 2 puntos del plano, de manera intercalada.
+
+Mostrar la creación del package filter y sus contenidos.
 
 5. Agrege las pruebas correspondientes a cada uno de estos filtros, y pruebe su funcionamiento en el programa de prueba, comprobando que sólo cambiando la posición de las anotaciones -sin cambiar nada más-, el programa retorne los planos filtrados de la manera (A) o de la manera (B). 
--- laboratorio/src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ laboratorio/src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -5,6 +5,7 @@
  */
 package edu.eci.arsw.blueprints.services;
 
+import edu.eci.arsw.blueprints.filter.BlueprintsFilter;
 import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
@@ -24,6 +25,9 @@ public class BlueprintsServices {
    
     @Autowired
     BlueprintsPersistence bpp;
+
+    @Autowired
+    BlueprintsFilter filter = null;
     
     public void addNewBlueprint(Blueprint bp){
         try {
@@ -32,9 +36,15 @@ public class BlueprintsServices {
             throw new RuntimeException("Error al agregar el blueprint" + e.getMessage(), e);
         }
     }
-    
+
     public Set<Blueprint> getAllBlueprints() throws BlueprintNotFoundException {
-        return bpp.getAllBlueprints();
+        Set<Blueprint> blueprints = bpp.getAllBlueprints();
+        // aplicar el filtro a cada uno
+        Set<Blueprint> filtered = new java.util.HashSet<>();
+        for (Blueprint bp : blueprints) {
+            filtered.add(filter.filter(bp));
+        }
+        return filtered;
     }
     
     /**
@@ -44,9 +54,14 @@ public class BlueprintsServices {
      * @return the blueprint of the given name created by the given author
      * @throws BlueprintNotFoundException if there is no such blueprint
      */
-    public Blueprint getBlueprint(String author,String name) throws BlueprintNotFoundException{
-        return bpp.getBlueprint(author,name);
+    public Blueprint getBlueprint(String author, String name) throws BlueprintNotFoundException {
+        Blueprint bp = bpp.getBlueprint(author, name);
+        if (bp == null) {
+            throw new BlueprintNotFoundException("Blueprint no encontrado: " + author + " - " + name);
+        }
+        return filter.filter(bp);
     }
+
     
     /**
      * 
@@ -54,8 +69,13 @@ public class BlueprintsServices {
      * @return all the blueprints of the given author
      * @throws BlueprintNotFoundException if the given author doesn't exist
      */
-    public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException{
-        return bpp.getBlueprintsByAuthor(author);
+    public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
+        Set<Blueprint> blueprints = bpp.getBlueprintsByAuthor(author);
+        Set<Blueprint> filtered = new java.util.HashSet<>();
+        for (Blueprint bp : blueprints) {
+            filtered.add(filter.filter(bp));
+        }
+        return filtered;
     }
     
 }
